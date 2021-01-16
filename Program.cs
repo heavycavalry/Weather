2afcb8ff6d5851ddb519c2ec6a503495a165e296
heavycavalry <klaudia.diana.paluch@gmail.com>
2021-01-16 11:50:18 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Weather
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var url = "https://api.openweathermap.org/data/2.5/weather?q=Krakow&appid=db73877da80bcf86ce14d0db9c8b06a3";
+            using var client = new HttpClient();
+
+            var content = await client.GetStringAsync(url);
+            var weatherForecast = JsonSerializer.Deserialize<WeatherForecast>(content);
+            float xd = weatherForecast.coord.lon;
+        }
+    }
+}
