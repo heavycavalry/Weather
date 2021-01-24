8ea42fbd2aafffcf865b26929fcf2051baf188ad
heavycavalry <klaudia.diana.paluch@gmail.com>
2021-01-24 01:40:52 +0100

    commands

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Channels;
 
 namespace Weather {
     class Program {
@@ -18,21 +19,28 @@ namespace Weather {
         public WeatherBroadcast _broadcast = new WeatherBroadcast();
 
         public void Run() {
+            Console.WriteLine();
+            Console.WriteLine("Hello Dear User. Welcome to our Weather App.");
+            Console.WriteLine("With our app you can choose what city and what weather you want to observe.");
+            Console.WriteLine("When the weather would by the same as the chosen you will be notified.");
+            Console.WriteLine("To see the commands enter help");
+            Console.WriteLine();
             while (true) {
                 try {
-                    Console.Write(">");
+                    Console.Write("> ");
                     string command = Console.ReadLine();
                     HandleUserInput(command);
+                    Console.WriteLine();
                 }
                 catch (Exception e) {
-                    Console.WriteLine(e);
+                    Console.WriteLine("Wrong command. Please try again.");
                 }
             }
         }
         
         public void HandleUserInput(string input) {
             var inputParts = input.Split(" "); //Split nie mutuje, trzeba przypisać
-            
+
             if (inputParts[0] == "addUser" || inputParts[0] == "au") {
                 User user = new UserBuilder()
                     .SetWeatherBroadcast(_broadcast)
@@ -74,6 +82,18 @@ namespace Weather {
                 string userLogin = inputParts[1];
                 User user = _users[userLogin];
                 user.SetStrategy(StrategyParser.ParseStrategy(inputParts[2]));
+                return;
+            }
+
+            if (inputParts[0] == "help") {
+                Console.WriteLine("addUser|ad <login name> - add a new user");
+                Console.WriteLine("removeUser|ru <login name> - remove user");
+                Console.WriteLine("subscribe|sub <login name> <city name> - subscribe city");
+                Console.WriteLine("unsubscribe|unsub <login name> <city> - unsubscribe city");
+                Console.WriteLine("setStrategy|ss <login name> <STRATEGY> - choose the weather");
+                Console.WriteLine();
+                Console.WriteLine("STRATEGY - Mist, Rain, Snow, Thunderstorm, Clear, Clouds, Ash, ");
+                Console.WriteLine("Haze, Smoke, Fog, Dust, Sand, Tornado, Squall, Drizzle");
                 return;
             }
 
